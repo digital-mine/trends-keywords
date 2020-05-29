f9ee36f21ece97f9b92f42107061b2bc74e44ca0
DIGITAL MINE <digitalmine23@gmail.com>
2020-05-29 13:18:01 +0100

    Update trends.py

--- trends.py
+++ trends.py
@@ -1,1 +1,10 @@
+import pandas as pd                        
+from pytrends.request import TrendReq
+from collections import Counter
+import time
 
+pt = TrendReq()
+def tr(key_w):
+    kw_list=[key_w]
+    print (key_w)
+    pt.build_payload(kw_list, cat=0, timeframe='now 7-d', geo='', gprop='')
